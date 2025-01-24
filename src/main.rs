dabb26e90f1360a89a02f674ce13d28dfe7bdf46
Thuyen Code <thuyencode@proton.me>
2025-01-24 12:01:08 +0700

    Return error

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,19 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).unwrap();
+    let result = std::fs::read_to_string(&args.path);
+
+    let content = match result {
+        Ok(content) => content,
+        Err(error) => return Err(error.into()),
+    };
 
     println!("File content:");
     println!("{}", content);
+
+    Ok(())
 
     // for line in content.lines() {
     //     if line.contains(&args.pattern) {
