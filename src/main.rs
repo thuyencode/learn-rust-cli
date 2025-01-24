3448fc4e7bfc2357ebd8b44c469eb5418a5edf61
Thuyen Code <thuyencode@proton.me>
2025-01-24 10:17:45 +0700

    CLI arguments as data type

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use std::env::args;
+use std::path::PathBuf;
+
+struct Cli {
+    pattern: String,
+    path: PathBuf,
+}
 
 fn main() {
     let pattern = args().nth(1).expect("no pattern given");
     let path = args().nth(2).expect("no path given");
 
-    println!("pattern: {:?}, path: {:?}", pattern, path)
+    let args = Cli {
+        pattern,
+        path: PathBuf::from(path),
+    };
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
