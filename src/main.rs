fd27d78f6252658d3495a92d6052d7ba954ec79f
Thuyen Code <thuyencode@proton.me>
2025-01-24 10:34:14 +0700

    Read file's content

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use std::path::PathBuf;
 
 // Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -7,11 +6,16 @@ struct Cli {
     // The pattern to look for
     pattern: String,
     // The path to the file to read
-    path: PathBuf,
+    path: std::path::PathBuf,
 }
 
 fn main() {
     let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
