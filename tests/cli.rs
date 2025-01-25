a29edafb909f6376237d3ee0a80d0e918e0ddfbf
Thuyen Code <thuyencode@proton.me>
2025-01-25 11:30:17 +0700

    Test the CLI app by running it

---
+++ tests/cli.rs
@@ -0,0 +1,16 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::error;
+use std::process::Command;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn error::Error>> {
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
